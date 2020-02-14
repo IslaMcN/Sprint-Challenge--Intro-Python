2a3f7bfec20a4edb3a74902c2a9e54ae7f3d8cff
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2020-02-14 08:26:24 -0800

    oop1 is finished

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,30 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    pass
+    ##Parent
+class FlightVehicle(Vehicle):
+    pass
+    ##Inherits from vehicle
+    ##FlightVehicle is-a vehicle
+class Starship(FlightVehicle):
+    pass
+    ##Inherits from FlightVehicle
+    ##Starship is-a FlightVehicle
+class GroundVehicle(Vehicle):
+    pass
+    ##Inherits from Vehicle
+    ##GroundVehicle is-a vehicle
+class Airplane(FlightVehicle):
+    pass
+    ##Inherits from FlightVehicle
+    ##Airplane is-a FlightVehicle
+class Car(GroundVehicle):
+    pass
+    ##Inherits from GroundVehicle
+    ##Car is-a GroundVehicle
+class Motorcycle(GroundVehicle):
+    pass
+    ##Inherits from GroundVehicle
+    ##Motorcycle is-a GroundVehicle
